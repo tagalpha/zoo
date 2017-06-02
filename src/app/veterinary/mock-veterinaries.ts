8c3dc2b5bb85c5592fc9b9e8cb97fe3b3050d857
Camille Chevrain <Camille@MacBook-de-Camille.local>
2017-06-02 14:43:48 +0200

    [FIX]Design + Mock

--- src/app/veterinary/mock-veterinaries.ts
+++ src/app/veterinary/mock-veterinaries.ts
@@ -3,5 +3,7 @@ import { Veterinary } from './veterinary';
 export const VETERINARIES: Veterinary[] = [
     { id: 1, name: 'Nicolas Blanchet', lat: 48.876667, lng :2.173333},
     { id: 2, name: 'Camille Chevrain', lat: 48.876667, lng :2.363333},
-    { id: 3, name: 'Alexandre Vettraino', lat: 48.839667, lng :2.693333},
+    { id: 3, name: 'Alexandre Vettraino', lat: 41.59667, lng :13.82333},
+    { id: 4, name: 'Bruno Ramos', lat: 16.8152800, lng :95.7561100},
+    { id: 5, name: 'Jolan Levy', lat: 14.6937, lng :-17.06406},
 ];
